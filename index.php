ffbc5169979342382a95072be40356623e7b2bba
anes89cok <anes89cok@gmail.com>
2015-03-26 09:55:14 +0100

    official site,and font-size

--- index.php
+++ index.php
@@ -94,7 +94,7 @@
 		    <h1><img src="img/logo.png" alt="LOGO"></h1>
 		</div>
 		<div data-role="footer" data-theme="<?php echo $theme ?>" data-position="fixed">
-		    <h1 style="font-weight: normal; font-size: 12px;">Powered By <a href="http://marketrivals.com" rel="external" style="text-decoration: none;">MobileGrows</a> | <a href="http://marketrivals.com" rel="external" style="text-decoration: none;">  Desktop Site</a></h1>
+		    <h1 style="font-weight: normal; font-size: 13px;">Powered By <a href="http://mobilegrows.com/" rel="external" style="text-decoration: none;">MobileGrows</a> | <a href="http://marketrivals.com" rel="external" style="text-decoration: none;">  Desktop Site</a></h1>
 		</div>
 		<!-- page 1 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
 		<div data-role="page" id="page1" data-theme="<?php echo $theme ?>">
